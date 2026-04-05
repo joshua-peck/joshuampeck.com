fe2203820ba7d6cfcd9590c88ab8c1620155f7a0
Josh Peck <josh@truecodecapital.com>
2026-04-04 23:45:16 -0600

    Adding updating infra/ from template to new project name

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,7 +13,7 @@ variable "env" {
 variable "domain" {
   description = "Primary domain (no www)"
   type        = string
-  default     = "virtualbusinessmailbox.com"
+  default     = "joshuampeck.com"
 }
 
 variable "region" {
@@ -25,19 +25,4 @@ variable "region" {
 locals {
   # Sanitize domain for use in resource names
   name_prefix = replace(var.domain, ".", "-")
-}
-
-variable "stripe_secret_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "stripe_webhook_secret" {
-  type      = string
-  sensitive = true
-}
-
-variable "teams_webhook_url" {
-  type      = string
-  sensitive = true
 }
